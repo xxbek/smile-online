858fad5495911582c95ab9bf5f67dab4b71bad1b
xxbek <kika.aubekerow@gmail.com>
2023-07-20 16:56:33 +0300

    change setting and req

--- smile_online/settings/production.py
+++ smile_online/settings/production.py
@@ -3,6 +3,6 @@ from .default import *
 
 
 SECRET_KEY = 'django-insecure-l%4nqv30_5eppo@b(%hvme#_4f-sl)kds442*o6ki1et50_0*z'
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ovz1.j2521725.m7o9p.vps.myjino.ru']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ovz1.j2521725.m7o9p.vps.myjino.ru', 'anketa-smileonline.ru']
